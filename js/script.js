2c34b646d09b16d426e9e2d073d2a7343a2e0d6d
Spyros Tomaras <sptoma724@gmail.com>
2020-06-09 16:17:36 +0300

    Scroll Down Effect on Characteristics

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+/*
+    We target a window object and we would attach an event listener for a load event
+    Load event for the window object is going to happen once the images and scripts have been loaded
+*/ 
 
+window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'))
 
-
-
+$(document).ready(function(){
+    $(".js--scroll-to-char").click(function (){
+        $('html, body').animate({scrollTop: $('.js--section-chars').offset().top}, 1000);
+    });
+})
